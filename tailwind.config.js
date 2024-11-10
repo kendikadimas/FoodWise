6dc098eae18691ac8d91f205fe37fdff66c4319e
kendikadimas <dimas.fazrulfalah@mhs.unsoed.ac.id>
2024-11-10 16:09:23 +0700

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: {
+    relative : true,
+    files: [
+    "./src/**/*.{html,js}",
+  ],},
   theme: {
     extend: {},
   },
